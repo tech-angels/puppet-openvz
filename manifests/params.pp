a5191da95d08156a62a0d27238046c4453e7c2fc
Gilbert Roulot <gilbert.roulot@tech-angels.com>
2012-11-19 22:29:56 +0100

    Adds a vzexec resource.

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,6 +3,11 @@ class openvz::params {
   $openvz_conf_dir = "/etc/vz/conf"
   $openvz_conf_file = "/etc/vz/vz.conf"
 
+  $openvz_private_dir = $lsbdistid ? {
+    /(Centos|RedHat)/ => '/vz/private',
+    Debian            => '/var/lib/vz/private',
+  }
+
   # OS dependant actions
   $fix_hosts = $lsbdistid ? {
     /(CentOS|RedHat)/ => true,
---
+++ manifests/vzctl-exec.pp
@@ -0,0 +1,78 @@
+/*
+
+== Definition: openvz::exec
+Executes a command in container
+
+Arguments:
+- *veid*:        container ID
+- *command*:     command to execute
+- *creates*
+- *cwd*
+- *environment*
+- *logoutput*
+- *onlyif*
+- *refresh*
+- *refreshonly*
+- *returns*
+- *timeout*
+- *tries*
+- *try_sleep*
+- *unless*
+
+Examples:
+
+openvz::exec {'do nothing in 101':
+  veid       => '101',
+  command    => '/bin/true',
+}
+*/
+define openvz::vzctl-exec (
+  $veid,
+  $command,
+  $creates = false,
+  $logoutput = on_failure,
+  $onlyif = false,
+  $refresh = false,
+  $refreshonly = false,
+  $returns = 0,
+  $timeout = 300,
+  $tries = 1,
+  $try_sleep = false,
+  $unless = false
+) {
+  include openvz::params
+  $openvz_private_dir = $openvz::params::openvz_private_dir
+
+  $real_creates = $creates ? {
+    false   => undef,
+    default => "${openvz_private_dir}/${creates}"
+  }
+
+  $real_onlyif = $onlyif ? {
+    false   => undef,
+    default => "vzctl exec2 ${veid} ${onlyif}"
+  }
+
+  $real_refresh = $refresh ? {
+    false   => undef,
+    default => "vzctl exec2 ${veid} ${refresh}"
+  }
+
+  $real_unless = $unless ? {
+    false   => undef,
+    default => "vzctl exec2 ${veid} ${unless}"
+  }
+
+  exec { "vzctl exec2 ${veid} ${command}":
+    creates     => $real_creates,
+    logoutput   => $logoutput,
+    onlyif      => $real_onlyif,
+    refresh     => $real_refresh,
+    refreshonly => $refreshonly,
+    returns     => $returns,
+    timeout     => $timeout,
+    tries       => $tries,
+    try_sleep   => $try_sleep ? { false => undef, default => $try_sleep },
+    unless      => $real_unless,
+  }
+}
